4a12f7364c9159430047701859951848639f7a21
LovreCikojevic120 <56341053+LovreCikojevic120@users.noreply.github.com>
2021-03-22 17:27:54 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bezveze");
+            Console.WriteLine("Bezveze");//ciki was here
         }
     }
 }
